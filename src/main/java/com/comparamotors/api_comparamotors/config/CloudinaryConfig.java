f3b0cb4a4597fca5f96812450b48f828a81c4340
ypradol <ypradol@ufpso.edu.co>
2025-02-08 21:26:47 -0500

    Se actualizan las clases para advertisement en base a news, ya que esta sigue la logica de la misma

--- src/main/java/com/comparamotors/api_comparamotors/config/CloudinaryConfig.java
+++ src/main/java/com/comparamotors/api_comparamotors/config/CloudinaryConfig.java
@@ -4,8 +4,6 @@ import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
@@ -21,7 +19,6 @@ public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary() {
-        // Usar el formato URL de Cloudinary directamente
         String cloudinaryUrl = String.format("cloudinary://%s:%s@%s", 
             apiKey, apiSecret, cloudName);
         return new Cloudinary(cloudinaryUrl);
